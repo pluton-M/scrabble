696a42561a02c5dc8b749a67c0c5d979bd6867d9
maia <81334143+pluton-M@users.noreply.github.com>
2022-08-23 18:15:38 -0300

    Update tab-scrabble(prototipo).cpp

--- tab-scrabble(prototipo).cpp
+++ tab-scrabble(prototipo).cpp
@@ -19,18 +19,25 @@ int main(){
 	
 	
 }
-/*
+/*tenemos que lograr:
+-que el usuario posiciones sus fichas
+-marcar los simbolos matematicos en el orden correspondiente a los valores que da el usuario
+-incluirlo en el otro codigo
+-que las fichas de ambos usuarios interactuen entre si
+*/
+
+/*NO BORRAR
 #include<bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-	vector< vector<int> > a (15, vector<int> (15, 0));
+	vector< vector<int> > a (15, vector<int> (15, 0)); // es un array de un array (una matriz)
 	
-	for(int i = 1; i <= 15; i++) cout << "|" << i << "| ";
+	for(int i = 1; i <= 15; i++) cout << "|" << i << "| "; // linea del tablero separada del for para indicar los numeros de las columnas
 	cout << endl;
-	for(int i = 1;  i <= 15; i++){
-		cout << "|" << i << "| ";
+	for(int i = 1;  i <= 15; i++){ // empieza el for para el tablero
+		cout << "|" << i << "| "; //para que antes de todas las filas las numere
 		for(int j = 1; j <= 15; j++){
 			cout << "|" << a[i][j] << "| ";
 		}
